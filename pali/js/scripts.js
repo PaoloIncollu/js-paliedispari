0a74957dac912a8c87de3aba3830b833579e40fa
PaoloIncollu <paoloinco@gmail.com>
2024-07-19 18:00:50 +0200

    caricamento esercizio pali

--- pali/js/scripts.js
+++ pali/js/scripts.js
@@ -1,1 +1,36 @@
 
+/*step 
+
+    1. Creare un prompt dove si chiede all'utente di inserire una parola
+    2. Creare una funzione per controllare se la parola inserita è palindroma
+    3. Dare un return in base alla parola
+        - se la parola e palindroma result true 
+        - altrimenti return false
+
+
+*/
+
+
+// step 1
+
+const word = prompt(' inserisci una parola');
+console.log(word);
+
+//step 2
+
+let charactersWord = word.split('');
+let wordReverse = charactersWord.reverse().join('');
+
+//step 3
+    if(word == wordReverse){
+
+        console.log ('La parola inserita è palindroma');
+    }
+
+    else{
+        console.log ('La parola inserita non è palindroma');
+    }
+    
+   
+
+    
